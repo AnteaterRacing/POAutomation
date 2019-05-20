96e2c2c0127fe775ecadf453914b674d362f4ed8
Vincent Ha <Vincent_Ha@dhcp-v028-196.mobile.uci.edu>
2019-05-19 17:23:04 -0700

    Added EmailDraft and its unit tests

---
+++ src/test/java/EmailDraftTest.java
@@ -0,0 +1,154 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+import java.util.List;
+
+import javax.mail.MessagingException;
+
+import com.google.api.services.gmail.model.Draft;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+class EmailDraftTest {
+    public static GmailAPI apis;
+    public static EmailDraft email;
+    public static AlreadySentException exception = new AlreadySentException("exception");
+    public static final String NEW_ADDRESS_FROM = "vguy77@gmail.com";
+    public static final String NEW_ADDRESS_TO = "vqha@uci.edu";
+    public static final String NEW_SUBJECT = "TEST2";
+    public static final String NEW_MESSAGE_BODY = "MESSAGE";
+
+    @BeforeAll
+    public static void setUp() throws IOException, GeneralSecurityException, MessagingException {
+        apis = GmailAPI.getInstance();
+        email = new EmailDraft("vqha@uci.edu", "vguy77@gmail.com", "test", "test", "me");
+    }
+
+    @Test
+    public void testUpdateAddressFrom() throws IOException, MessagingException, AlreadySentException {
+        email.updateAddressFrom(NEW_ADDRESS_FROM);
+
+        List<Draft> drafts = apis.getDrafts("me");
+        Draft myDraft = null;
+        for (Draft d : drafts) {
+            if (d.getId().equals(email.getDraftID())) {
+                myDraft = d;
+                break;
+            }
+        }
+
+        // function should have made a draft
+        assertNotEquals(null, myDraft);
+
+        // The two drafts should be the same
+        assertEquals(email.getDraftID(), myDraft.getId());
+    }
+
+    @Test
+    public void testUpdateAddressTo() throws IOException, MessagingException, AlreadySentException {
+        email.updateAddressTo(NEW_ADDRESS_TO);
+
+        List<Draft> drafts = apis.getDrafts("me");
+        Draft myDraft = null;
+        for (Draft d : drafts) {
+            if (d.getId().equals(email.getDraftID())) {
+                myDraft = d;
+                break;
+            }
+        }
+
+        // function should have made a draft
+        assertNotEquals(null, myDraft);
+
+        // The two drafts should be the same
+        assertEquals(email.getDraftID(), myDraft.getId());
+    }
+
+    @Test
+    public void testUpdateAddressSubjectLine() throws IOException, MessagingException, AlreadySentException {
+        email.updateSubjectLine(NEW_SUBJECT);
+
+        List<Draft> drafts = apis.getDrafts("me");
+        Draft myDraft = null;
+        for (Draft d : drafts) {
+            if (d.getId().equals(email.getDraftID())) {
+                myDraft = d;
+                break;
+            }
+        }
+
+        // function should have made a draft
+        assertNotEquals(null, myDraft);
+
+        // The two drafts should be the same
+        assertEquals(email.getDraftID(), myDraft.getId());
+    }
+
+    @Test
+    public void testUpdateMessage() throws IOException, MessagingException, AlreadySentException {
+        email.updateMessage(NEW_MESSAGE_BODY);
+
+        List<Draft> drafts = apis.getDrafts("me");
+        Draft myDraft = null;
+        for (Draft d : drafts) {
+            if (d.getId().equals(email.getDraftID())) {
+                myDraft = d;
+                break;
+            }
+        }
+
+        // function should have made a draft
+        assertNotEquals(null, myDraft);
+
+        // The two drafts should be the same
+        assertEquals(email.getDraftID(), myDraft.getId());
+    }
+
+    @Test
+    void checkAge() {
+        int age = email.checkAgeinDays();
+        assertEquals(0, age);
+    }
+
+    @Test
+    void checkSendingEmail() throws IOException {
+        email.sendDraft();
+
+        // check if draft is sent
+        assertTrue(email.hasBeenSent());
+
+        // check if we cannot update addressFrom
+        Exception exception = assertThrows(AlreadySentException.class, () -> {
+            email.updateAddressFrom(NEW_ADDRESS_FROM);
+        });
+        assertEquals(String.format("CANNOT EDIT ADDRESSFROM. EMAIL %s ALREADY SENT", email.getEmailID()),
+                exception.getMessage());
+
+        // check if we cannot update addressTo
+        exception = assertThrows(AlreadySentException.class, () -> {
+            email.updateAddressTo(NEW_ADDRESS_TO);
+        });
+        assertEquals(String.format("CANNOT EDIT ADDRESSTO. EMAIL %s ALREADY SENT", email.getEmailID()),
+                exception.getMessage());
+
+        // check if we cannot update Subject
+        exception = assertThrows(AlreadySentException.class, () -> {
+            email.updateSubjectLine(NEW_SUBJECT);
+        });
+        assertEquals(String.format("CANNOT EDIT SUBJECT. EMAIL %s ALREADY SENT", email.getEmailID()),
+                exception.getMessage());
+
+        // check if we cannot update Message
+        exception = assertThrows(AlreadySentException.class, () -> {
+            email.updateMessage(NEW_MESSAGE_BODY);
+        });
+        assertEquals(String.format("CANNOT EDIT MESSAGEBODY. EMAIL %s ALREADY SENT", email.getEmailID()),
+                exception.getMessage());
+    }
+
+}
--- src/test/java/SpreadSheetTest.java
+++ src/test/java/SpreadSheetTest.java
@@ -1,3 +1,5 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -10,10 +12,17 @@ import java.util.List;
 
 class SpreadSheetTest
 {
+    private SpreadSheet testSheet;
+
+    @BeforeEach
+    public void setupSheets() throws IOException, GeneralSecurityException
+    {
+        testSheet = new SpreadSheet("test1");
+    }
+
     @Test
     public void testWrite() throws IOException, GeneralSecurityException
     {
-        SpreadSheet sheet = new SpreadSheet("test");
         List<List<Object>> values  = new ArrayList<>();
 
         values.add(Arrays.asList(
@@ -29,28 +38,27 @@ class SpreadSheetTest
                         Integer.toString(ThreadLocalRandom.current().nextInt(0, 10)), 
                         Integer.toString(ThreadLocalRandom.current().nextInt(0, 10))));
 
-        sheet.write("Sheet1", values, false);
-        List<List<Object>> results = sheet.getValues("Sheet1");
+        testSheet.write("Sheet1", values, false);
+        List<List<Object>> results = testSheet.getValues("Sheet1");
         
         assertEquals(values, results);
-        DriveAPI drive = DriveAPI.getDriveAPI();
-        drive.deleteItem(sheet.getID());
     }
 
     @Test
     public void testCopyConstructor() throws IOException, GeneralSecurityException
     {
-        SpreadSheet sheet = new SpreadSheet("test");
-        SpreadSheet copy = new SpreadSheet(sheet, "test2");
+        SpreadSheet copy = new SpreadSheet(testSheet, "test2");
 
-        List<List<Object>> sheetResults = sheet.getValues("Sheet1");
+        List<List<Object>> sheetResults = testSheet.getValues("Sheet1");
         List<List<Object>> copyResults = copy.getValues("Sheet1");
 
         assertEquals(sheetResults, copyResults);
-        DriveAPI drive = DriveAPI.getDriveAPI();
-        drive.deleteItem(sheet.getID());
-        drive.deleteItem(copy.getID());
     }
 
-    
+    @AfterEach
+    public void cleanUp() throws IOException, GeneralSecurityException
+    {
+        DriveAPI drive = DriveAPI.getDriveAPI();
+        drive.deleteItem(testSheet.getID());
+    }
 }
