17200a6d733d6854d5073dea2513258877d84ded
Vincent Ha <Vincent_Ha@MacBook-Pro.local>
2019-05-25 00:39:40 -0700

    Fixed SheetsAPITest to remove created spreadsheet from unit testing.

--- src/test/java/SheetsAPITest.java
+++ src/test/java/SheetsAPITest.java
@@ -7,18 +7,38 @@ import java.util.concurrent.ThreadLocalRandom;
 import com.google.api.services.drive.model.File;
 
 import java.util.List;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class SheetsAPITest {
-    // TODO: Add unit test for Renaming Tabs
+    public static final String name = "Stuff";
+    public static final String valueOption = "RAW";
+    public static String range = "Sheet1!A1:C3";
+    public static SheetsAPI sheetDriver;
+    public static String id;
+
+    @BeforeEach
+    public void setUp() throws IOException, GeneralSecurityException {
+        sheetDriver = SheetsAPI.getSheetsAPI();
+        id = sheetDriver.createSpreadsheet(name);
+
+        List<List<Object>> values = new ArrayList<>();
+        values.add(Arrays.asList(1, 0, 0));
+        values.add(Arrays.asList(0, 1, 0));
+        values.add(Arrays.asList(0, 0, 1));
+
+        sheetDriver.writeToSpreadsheet(id, range, valueOption, values);
+    }
+
     @Test
     public void testCreatingSpreadsheet() throws IOException, GeneralSecurityException {
-        SheetsAPI sheet = SheetsAPI.getSheetsAPI();
         DriveAPI drive = DriveAPI.getDriveAPI();
-        String name = "Stuff";
-        String id = sheet.createSpreadsheet(name);
+        String id = sheetDriver.createSpreadsheet(name);
 
         List<File> files = drive.getFiles();
         boolean seenFile = false;
@@ -37,10 +57,7 @@ class SheetsAPITest {
 
     @Test
     public void testGettingValues() throws IOException, GeneralSecurityException {
-        String id = "1VhNsnBAiF1U8ifXr3u0kdO-iC_HOG56h2AZ9zNnmzuE";
-        String range = "Test0!A1:C3";
-        SheetsAPI sheet = SheetsAPI.getSheetsAPI();
-        List<List<Object>> sheetValues = sheet.getValues(id, range);
+        List<List<Object>> sheetValues = sheetDriver.getValues(id, range);
 
         List<List<Object>> expectedValues = new ArrayList<>();
         expectedValues.add(Arrays.asList("1", "0", "0"));
@@ -51,26 +68,45 @@ class SheetsAPITest {
     }
 
     @Test
+    public void testRenamingTabs() throws IOException, GeneralSecurityException {
+        sheetDriver.renameTab(id, 0, "Test0");
+        
+        // Check if old tab name exists
+        assertThrows(
+            IOException.class,
+            () -> { sheetDriver.getValues(id, range); }, 
+            "Did not throw IOException as expected");
+
+        range = "Test0!A1:C3";
+        testGettingValues();
+    }
+
+    @Test
     public void testWriteToSpreadsheet() throws IOException, GeneralSecurityException {
-        String id = "1VhNsnBAiF1U8ifXr3u0kdO-iC_HOG56h2AZ9zNnmzuE";
-        String range = "Test1!A1:C3";
-        String valueOption = "RAW";
         List<List<Object>> values = new ArrayList<>();
 
-        values.add(Arrays.asList(Integer.toString(ThreadLocalRandom.current().nextInt(0, 10)),
-                Integer.toString(ThreadLocalRandom.current().nextInt(0, 10)),
-                Integer.toString(ThreadLocalRandom.current().nextInt(0, 10))));
-        values.add(Arrays.asList(Integer.toString(ThreadLocalRandom.current().nextInt(0, 10)),
-                Integer.toString(ThreadLocalRandom.current().nextInt(0, 10)),
-                Integer.toString(ThreadLocalRandom.current().nextInt(0, 10))));
-        values.add(Arrays.asList(Integer.toString(ThreadLocalRandom.current().nextInt(0, 10)),
-                Integer.toString(ThreadLocalRandom.current().nextInt(0, 10)),
-                Integer.toString(ThreadLocalRandom.current().nextInt(0, 10))));
+        values.add(Arrays.asList(
+            Integer.toString(ThreadLocalRandom.current().nextInt(0, 10)),
+            Integer.toString(ThreadLocalRandom.current().nextInt(0, 10)),
+            Integer.toString(ThreadLocalRandom.current().nextInt(0, 10))));
+        values.add(Arrays.asList(
+            Integer.toString(ThreadLocalRandom.current().nextInt(0, 10)),
+            Integer.toString(ThreadLocalRandom.current().nextInt(0, 10)),
+            Integer.toString(ThreadLocalRandom.current().nextInt(0, 10))));
+        values.add(Arrays.asList(
+            Integer.toString(ThreadLocalRandom.current().nextInt(0, 10)),
+            Integer.toString(ThreadLocalRandom.current().nextInt(0, 10)),
+            Integer.toString(ThreadLocalRandom.current().nextInt(0, 10))));
 
-        SheetsAPI sheet = SheetsAPI.getSheetsAPI();
-        sheet.writeToSpreadsheet(id, range, valueOption, values);
-        List<List<Object>> results = sheet.getValues(id, range);
+        sheetDriver.writeToSpreadsheet(id, range, valueOption, values);
+        List<List<Object>> results = sheetDriver.getValues(id, range);
 
         assertEquals(values, results);
+    }
+
+    @AfterEach
+    public void cleanUp() throws IOException, GeneralSecurityException {
+        DriveAPI driveHandler = DriveAPI.getDriveAPI();
+        driveHandler.deleteItem(id);
     }
 }
